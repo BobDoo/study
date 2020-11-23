c04283d8b2dbb9f2ac01b498077589a3b2701f81
BobDoo <black9003@nate.com>
2020-11-23 22:01:07 +0900

    201123_study commit

---
+++ js/chap1_201121.js
@@ -0,0 +1,42 @@
+// 2020-11-21 자바스크립트 es6 기초
+
+// generator
+function* gen() {
+  let i = 0;
+
+  while (true) {
+    yield i++;
+  }
+}
+const range = gen();
+
+console.log("generator:", range.next());
+console.log("generator:", range.next());
+console.log("generator:", range.next());
+console.log("generator:", range.next());
+
+// promise
+const status = 2100;
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (status === 200) resolve("success");
+    else reject("error");
+  }, 0);
+});
+
+promise.then((res) => console.log(res)).catch((err) => console.log(err));
+
+// async await
+const fnc = async () => {
+  try {
+    const b = await promise;
+    console.log("async:", b);
+  } catch (e) {
+    console.log("async:", e);
+  }
+};
+
+fnc();
+
+console.log("asdf");
